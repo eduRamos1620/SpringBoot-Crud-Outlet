c93f6f6e2521dfe186f78584f67de9677189e9ad
eduRamos1620 <eduardoramosrosales16@gmail.com>
2025-05-25 10:33:37 -0600

    Se Crea el proyecto SpringBoot-Outlet

---
+++ src/main/java/com/ramos/springboot_outlet/SpringbootOutletApplication.java
@@ -0,0 +1,13 @@
+package com.ramos.springboot_outlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOutletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootOutletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramos/springboot_outlet/SpringbootOutletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramos.springboot_outlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOutletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
