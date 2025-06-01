d0fa3614b1f9b6f96686ab841817d8b1a9b51494
eduRamos1620 <eduardoramosrosales16@gmail.com>
2025-05-31 18:03:47 -0600

    Se crea la clase DetalleVentaController y su clase DetalleVentaDTO, CRUD basico

---
+++ src/main/java/com/ramos/springboot_outlet/controller/VentaController.java
@@ -0,0 +1,4 @@
+package com.ramos.springboot_outlet.controller;
+
+public class VentaController {
+}
---
+++ src/main/java/com/ramos/springboot_outlet/controller/dto/VentaDTO.java
@@ -0,0 +1,27 @@
+package com.ramos.springboot_outlet.controller.dto;
+
+import com.ramos.springboot_outlet.entities.Cliente;
+import com.ramos.springboot_outlet.entities.DetalleVenta;
+import com.ramos.springboot_outlet.entities.Usuario;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class VentaDTO {
+    private Long id;
+    private Long numeroVenta;
+    private Date diaVenta;
+    private BigDecimal total;
+    private String metodoPago;
+    private Usuario usuario;
+    private Cliente cliente;
+    private DetalleVenta detalle_venta;
+}
